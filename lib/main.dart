95d09ede0bc54a37ec6d33a200588a1080e5f669
logicadellee <adelleee1@gmail.com>
2026-09-10 22:37:55 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Praktikum Mobile Lanjut'),
+        ),
+        body: const Center(
+          child: Text('Halo Nama Saya Adelia'),
+        ),
+      ),
+    );
+  }
+}
